2db64777c366e958e084b1a4e0afb56fe72f6d53
mohamed mahmoud hanenne <medhanenne38@gmail.com>
2025-12-05 22:29:00 +0100

    edit

--- src/app/components/todoapp/todoapp.component.ts
+++ src/app/components/todoapp/todoapp.component.ts
@@ -12,18 +12,45 @@ import { FormsModule } from '@angular/forms';
 export class TodoappComponent {
   task ='';
   taskList:{id:number,name:string}[] = [];
+  
+  // Pour l'édition
+  editMode = false;
+  currentId: number | null  = null;
 
 
   addTask(){
-    this.taskList.push({id:this.taskList.length+1, name:this.task});
+
+    if(this.editMode && this.currentId !== null){
+      this.updateTask();
+      return;
+    }
+
+    this.taskList.push({
+    id:this.taskList.length+1, 
+    name:this.task});
+    this.task = "";
+  }
+
+
+  editTask(item: {id: number, name: string}){
+    this.editMode = true;
+    this.currentId = item.id;
+    this.task = item.name;
+  }
+
+  updateTask() {
+    this.taskList = this.taskList.map(item => {
+      if(item.id === this.currentId){
+        return { ...item, name:this.task}
+      }
+      return item;
+    });
+    this.editMode = false;
+    this.currentId = null;
     this.task = "";
   }
 
   deleteTask(id:number){
-    this.taskList = this.taskList.filter((item)=> item.id != id);
-  }
-
-  editTask(item:any){
-
+  this.taskList = this.taskList.filter((item)=> item.id != id);
   }
 }
